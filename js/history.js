b4fab4fcc0d329288612d1cb16e1e87f02a20e77
Zak Spence <zakspence@gmail.com>
2017-01-17 09:54:44 -0600

    add cornification

---
+++ js/history.js
@@ -0,0 +1,13 @@
+// var rainbowHeader = document.getElementById("double-rainbow").innerHTML;
+// console.log(rainbowHeader);
+// while(rainbowHeader) {
+// 	for (var i = 0; i < rainbowHeader.length; i++) {
+// }
+
+// var couleur = [
+
+// 	{
+		
+// 	}
+
+// ]
